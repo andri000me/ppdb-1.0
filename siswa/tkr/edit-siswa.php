4007baf9b5f2f93c2860aa9b2feca84209509ed9
kuriyantoadi <melatikuning212@gmail.com>
2020-06-02 19:53:36 +0700

    update 2 juni 2020, sesi 37

--- siswa/tkr/edit-siswa.php
+++ siswa/tkr/edit-siswa.php
@@ -39,7 +39,7 @@ if ($_SESSION['status']!="siswa") {
             <h5><b>Tahun Pelajaran 2020/2021</b></h4>
           </center>
           <center>
-            <h4><b>Program Studi Teknik Komputer Jaringan</b></h4>
+            <h4><b>Program Studi Teknik Kendaraan Ringan</b></h4>
           </center><br>
           <!-- font ganti jenis -->
         </div>
