6284c3334bb3f73c85b822d651c650b97d5a095d
kuriyantoadi <melatikuning212@gmail.com>
2020-06-02 15:52:23 +0700

    update 2 juni 2020, sesi 24

--- siswa/rpl/cetak-rpl-1.php
+++ siswa/rpl/cetak-rpl-1.php
@@ -25,7 +25,7 @@
           <center><h6><b>Bukti Pendaftaran Calon Peserta Didik Baru</b></h6></center>
           <center><h6><b>Calon Peserta Didik Baru</b></h6></center>
           <center><h6><b>Tahun Pelajaran 2020/2021</b></h6></center>
-          <center><h6><b>Program Studi Rekayasa Perangkat Lunak</b></h6></center><br>
+          <center><h6><b>Program Studi Teknik Komputer Jaringan</b></h6></center><br>
         </td>
         <td>
           <center><img style="margin-bottom:  80px; margin-top:  25px; margin-left: 90px" src="../../images/logo-smkn1-cetak.png" />
