e583ba65ba58095be4d3dd6a4430a13def8b0efb
kuriyantoadi <melatikuning212@gmail.com>
2020-06-13 16:22:40 +0700

    update 13 juni 2020, sesi 25

--- siswa/rpl/grafik.php
+++ siswa/rpl/grafik.php
@@ -224,7 +224,7 @@
             <?php
             //siswa sudah seleksi
             $diagram_periksa = mysqli_query($koneksi, "SELECT tgl_pendaftaran FROM f_siswa_rpl WHERE kondisi = ''");
-            echo $diagram_periksa;
+            echo mysqli_num_rows($diagram_periksa);
             ?>,
             <?php
             $diagram_periksa_cek = mysqli_num_rows($diagram_periksa);
