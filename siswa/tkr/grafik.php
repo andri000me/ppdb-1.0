2170cc8b0c3bafde4a7386b652167b125142813f
kuriyantoadi <melatikuning212@gmail.com>
2020-06-13 22:05:55 +0700

    update 13 juni 2020, sesi 41

--- siswa/tkr/grafik.php
+++ siswa/tkr/grafik.php
@@ -223,7 +223,8 @@
           ?>,
           <?php
           //siswa sudah seleksi
-          $diagram_periksa = mysqli_query($koneksi, "SELECT kondisi FROM f_siswa_tkr WHERE kondisi = ''");
+          // $diagram_periksa = mysqli_query($koneksi, "SELECT kondisi FROM f_siswa_tkr WHERE kondisi = '' and tgl_pendaftaran ='' ");
+          $diagram_periksa = mysqli_query($koneksi, "SELECT kondisi, tgl_pendaftaran FROM f_siswa_tkr WHERE kondisi = '' and tgl_pendaftaran IS NOT null and email !=''");
           echo mysqli_num_rows($diagram_periksa);
           ?>,
           <?php
