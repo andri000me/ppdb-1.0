a0aeccd73ffa7d53ddacaaad1b63578ae76c81c5
kuriyantoadi <melatikuning212@gmail.com>
2020-06-13 16:11:09 +0700

    update 13 juni 2020, sesi 19

--- siswa/tkr/grafik.php
+++ siswa/tkr/grafik.php
@@ -204,39 +204,33 @@
         datasets: [{
           label: '',
           data: [
-          <?php
-          //seluruh siswa
-          $diagram_semua = mysqli_query($koneksi, "SELECT tgl_pendaftaran FROM f_siswa_tkr ");
-          echo mysqli_num_rows($diagram_semua);
-          ?>,
-          <?php
-          //siswa belum seleksi
-          $diagram_blm = mysqli_query($koneksi, "SELECT tgl_pendaftaran FROM f_siswa_tkr WHERE tgl_pendaftaran = ''");
-          echo mysqli_num_rows($diagram_blm);
-          ?>,
-          <?php
-          //sudah seleksi
-          $diagram_semua_cek = mysqli_num_rows($diagram_semua);
-          $diagram_blm_cek = mysqli_num_rows($diagram_blm);
-          $sudah_seleksi = $diagram_semua_cek - $diagram_blm_cek;
-          echo $diagram_semua_cek - $diagram_blm_cek;
-          ?>,
-          <?php
-          //siswa sudah seleksi
-          $diagram_periksa = mysqli_query($koneksi, "SELECT tgl_pendaftaran FROM f_siswa_tkr WHERE kondisi = ''");
-          echo mysqli_num_rows($diagram_periksa);
-          ?>,
-          <?php
-          $diagram_periksa_cek = mysqli_num_rows($diagram_periksa);
-          // $akl_semua_cek = mysqli_num_rows($akl_semua);
-          echo $sudah_seleksi - $diagram_periksa_cek;
-          ?>,
-          <?php
-          //belum diperiksa
-          $diagram_periksa_cek = mysqli_num_rows($diagram_periksa);
-          // $akl_semua_cek = mysqli_num_rows($akl_semua);
-          echo $sudah_seleksi - $diagram_periksa_cek;
-          ?>
+            <?php
+            //seluruh siswa
+            $diagram_semua = mysqli_query($koneksi, "SELECT tgl_pendaftaran FROM f_siswa_tkr ");
+            echo mysqli_num_rows($diagram_semua);
+            ?>,
+            <?php
+            //siswa belum seleksi
+            $diagram_blm = mysqli_query($koneksi, "SELECT tgl_pendaftaran FROM f_siswa_tkr WHERE tgl_pendaftaran = ''");
+            echo mysqli_num_rows($diagram_blm);
+            ?>,
+            <?php
+            //sudah seleksi
+            $diagram_semua_cek = mysqli_num_rows($diagram_semua);
+            $diagram_blm_cek = mysqli_num_rows($diagram_blm);
+            $sudah_seleksi = $diagram_semua_cek - $diagram_blm_cek;
+            echo $diagram_semua_cek - $diagram_blm_cek;
+            ?>,
+            <?php
+            //siswa sudah seleksi
+            $diagram_periksa = mysqli_query($koneksi, "SELECT tgl_pendaftaran FROM f_siswa_tkr WHERE kondisi = ''");
+            echo mysqli_num_rows($diagram_periksa);
+            ?>,
+            <?php
+            $diagram_periksa_cek = mysqli_num_rows($diagram_periksa);
+            // $akl_semua_cek = mysqli_num_rows($akl_semua);
+            echo $sudah_seleksi - $diagram_periksa_cek;
+            ?>
           ],
           backgroundColor: [
           'rgba(0, 101, 153, 1.00)',
