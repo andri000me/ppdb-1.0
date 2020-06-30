fceb83e3d342ec50610de82163b1fcdcf032b49e
kuriyantoadi <melatikuning212@gmail.com>
2020-06-30 22:01:25 +0700

    update 30 juni 2020, sesi 22

---
+++ du/a/d/otkp/reset.php
@@ -0,0 +1,26 @@
+<?php
+// koneksi database
+include '../../../../koneksi.php';
+
+
+session_start();
+if ($_SESSION['status']!="otkp") {
+    header("location:../../index.php?pesan=belum_login");
+} else {
+    $nik = $_GET['nik'];
+
+    mysqli_query($koneksi, "UPDATE daftar_ulang SET
+               nik='$nik',
+               pdf_fakta='',
+               pdf_swa_fakta='',
+               pdf_akta='',
+               pdf_kk='',
+               pdf_photo='',
+               pdf_surat_dokter='',
+               pdf_skl=''
+               where nik='$nik'
+               ") or die(mysqli_error($koneksi));
+    ;
+
+    header("location:tampil.php?nik=$nik");
+}
